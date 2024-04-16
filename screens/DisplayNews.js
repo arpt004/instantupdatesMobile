ac310e9bfba4e1c188147a0bf8304ed84000c1db
Arpit Jain <arpt004@gmail.com>
2024-04-17 01:32:08 +0530

    Added notification funcationality using fcm token

--- screens/DisplayNews.js
+++ screens/DisplayNews.js
@@ -5,6 +5,7 @@ import * as Notifications from 'expo-notifications';
 import MainScreen from "../components/DisplayNews/MainScreen";
 import NoRecord from "../components/NoRecord/NoRecord";
 import Loader from "../components/Loader/Loader";
+import messaging from "@react-native-firebase/messaging";
 
 // import { newsData } from "../components/DisplayNews/news";
 // const cards = newsData
@@ -52,15 +53,55 @@ export default function DisplayNews() {
         fetchNewsData();
     },[category])
 
+    async function insert_push_key(pushToken) {
+
+        try{
+            // instantupdates.in/api/insert-push-key
+            // Notification Push token for expo in mobile : ExponentPushToken[BHj3CCMmxO2DxISmfM8Kwl]
+            // Notification Push token for android studio emulator : ExponentPushToken[25jUfPHtleWIrwD-jWVIXN]
+            // const deviceData = { pushKey : 'ExponentPushToken[25jUfPHtleWIrwD-jWVIXN]'}
+
+            const deviceData = { pushKey : pushToken}
+            const responseInsertPushKey = await fetch( `https://www.instantupdates.in/api/insert-push-key`, 
+                {
+                    method: 'POST',
+                    headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(deviceData),
+                },
+            );
+
+            if(responseInsertPushKey.ok){
+                console.log(responseInsertPushKey)
+            }
+          }catch(error) {
+            console.log(error);
+          }
+    }
+
+
     useEffect( () => {
         async function configurePushNotifications() {
+
+             // configure expo
             const { status } =  await Notifications.getPermissionsAsync();
             let finalStatus = status;
-
             if(finalStatus !== 'granted') {
                 const { status } =  await Notifications.requestPermissionsAsync();
                 finalStatus = status;
             }
+            
+
+
+            // // configure firebase
+            // const status =  await messaging().requestPermission();
+            // const enabled = status===messaging.AuthorizationStatus.AUTHORIZED || status===messaging.AuthorizationStatus.PROVISIONAL;
+
+            // // to get the expo push token
+            // const pushToken = await Notifications.getExpoPushTokenAsync();
+            // console.log(pushToken)
 
             if(finalStatus !== 'granted') {
                 Alert.alert(
@@ -70,23 +111,49 @@ export default function DisplayNews() {
                 return;
             }
 
-            // // to get the expo push token
-            // const pushToken = await Notifications.getExpoPushTokenAsync();
-            // console.log(pushToken)
+            console.log('pushToken')
+            const pushToken = await messaging().getToken();
+            console.log(pushToken)
+            if(pushToken) insert_push_key(pushToken)
 
+            // // expo default
             if (Platform.OS === 'android') {
                 Notifications.setNotificationChannelAsync('default', {
                     name: 'default',
                     importance: Notifications.AndroidImportance.DEFAULT
                 })
             }
+
+            // // check weather initial notification is available
+            // messaging().getInitialNotification().then( async (remoteMessage) => {
+            //     if(remoteMessage) {
+            //         console.log(" Notification caused app to open from quit", remoteMessage.notification)
+            //     }
+            // });
+
+            // // Assume a message contains a type property in the data payload of the screen
+            // messaging().onNotificationOpenedApp( (remoteMessage) => {
+            //     console.log(" Notification caused app to open from background", remoteMessage.notification)
+            // });
+
+            // // Register background handler
+            // messaging().setBackgroundMessageHandler( async (remoteMessage) => {
+            //     console.log("Mesaage handled in the background ", remoteMessage.notification)
+            // });
+
+            // //
+            // const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+            //     Alert.alert("A new message arrived", JSON.stringify(remoteMessage))
+            // })
+
+            // return unsubscribe;
         }
 
         configurePushNotifications();
-    })
+    },[])
 
 
-    // // Additional notification listener
+    // // Additional notification listener expo
     // useEffect(() =>{
     //     const subscriptionReceived = Notifications.addNotificationReceivedListener( (notification) => {
     //         return;
